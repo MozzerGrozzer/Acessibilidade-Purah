ca9cc86a33ba0c3cd978ab14c23b3d62908321ac
MozzerGrozzer <grozzermozzer@gmail.com>
2025-06-18 10:37:07 -0300

    evento super cool adicionado

--- src/js/curso.js
+++ src/js/curso.js
@@ -1,14 +1,33 @@
-  const finalizarBtn = document.getElementById('finalizarCursoBtn');
-  const popup = document.getElementById('popup');
-  const voltarHomeBtn = document.getElementById('voltarHomeBtn');
+  document.addEventListener("DOMContentLoaded", () => {
+    const finalizarBtn = document.getElementById("finalizarCursoBtn");
+    const popup = document.querySelector(".popup");
+    const body = document.body;
 
-  finalizarBtn.addEventListener('click', () => {
-    popup.classList.remove('hidden');
-    finalizarBtn.classList.add('hidden');
-    voltarHomeBtn.classList.remove('hidden');
-    document.body.classList.add('popup-active');
-  });
+    // Checagem se o evento aconteceu antes
+    if (localStorage.getItem("cursoFinalizado") === "true") {
+      // impedir replay do evento
+      if (finalizarBtn) finalizarBtn.remove();
+      document.getElementById("mensagemErro").style.display = "block";
+      if (popup) popup.remove();
+      body.classList.remove("popup-active");
+      return;
+    }
 
-  voltarHomeBtn.addEventListener('click', () => {
-    window.location.href = '../../index.html';
+
+
+    // Evento ao clicar no botão de finalizar o curso
+    if (finalizarBtn) {
+      finalizarBtn.addEventListener("click", () => {
+        // evento aconteceu
+        localStorage.setItem("cursoFinalizado", "true");
+
+        //trava scroll
+        body.classList.add("popup-active");
+        if (popup) popup.classList.add("active");
+
+        setTimeout(() => {
+          window.location.href = "../../index.html"; 
+        }, 4000); // tempo de exibição mudar
+      });
+    }
   });
